47b71d318beb808f8e25153b9a10f3ab6f359901
tastuya1002 <tatsu.sf1002@gmail.com>
2024-06-22 11:22:37 +0900

    CORSエラーの対応 - 違うオリジンでもAPIが叩けるように対応した - lombokの追加

--- src/main/java/com/example/kakeibo/controller/TestController.java
+++ src/main/java/com/example/kakeibo/controller/TestController.java
@@ -1,10 +1,11 @@
 package com.example.kakeibo.controller;
+import com.example.kakeibo.domain.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping(value="/test")
-    public void test() {
-        System.out.println("test!");
+    public Greeting test() {
+        return new Greeting("Taro", 28);
     }
 }
