39f47d1a7737b70419a2b7c3938352f9a1f8ab11
tastuya1002 <tatsu.sf1002@gmail.com>
2024-09-01 19:13:46 +0900

    ユーザー登録機能の実装

--- src/main/java/com/example/kakeibo/settings/CorsConfig.java
+++ src/main/java/com/example/kakeibo/settings/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.kakeibo.config;
+package com.example.kakeibo.settings;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
